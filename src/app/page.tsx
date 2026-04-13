3a6e93dba087adf51058f2ec66684572ac127875
ephraim64 <tettehephraim.64@gmail.com>
2026-04-13 09:45:05 +0000

    radio added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Video from "@/src/components/Video";
 import React from "react";
 import AppDownload from "@/src/components/App";
 import FeaturedProducts from "../components/FeaturedProducts";
+import Radio from "../components/Radio";
 
 const HomePage = () => {
   return (
@@ -14,7 +15,9 @@ const HomePage = () => {
       <div className="max-sm:relative w-full bg-white rounded-t-3xl shadow-[0_-8px_30px_rgba(0,0,0,0.08)]">
         <div className="w-full flex flex-col px-4 md:px-16 lg:px-14 xl:px-12">
           <FeaturedProducts category="apartments" />
+          <FeaturedProducts category="food" />
           <div className="items-center justify-center w-full gap-8 px-3 lg:px-12 mt-10 mb-5">
+              <Radio />
             <div className="w-full">
               <h1 className="lg:text-4xl font-bold py-12 capitalize text-center">
                 Promote your Business & products <br /> Live On smileBaba TV
@@ -23,8 +26,9 @@ const HomePage = () => {
             </div>
           </div>
 
-          <FeaturedProducts category="food" />
           <FeaturedProducts category="marketplace" />
+          <FeaturedProducts category="fashion" />
+          <FeaturedProducts category="pharmacy" />
         </div>
         <AppDownload />
       </div>
